b16d48bb6d145a0ce8c6d5cbcdd536d678702031
Robert Mahoney <robert.w.mahoney@gmail.com>
2018-10-29 23:27:29 -0400

    testing user calls

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UserService } from './services/user.service'
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'PRSng';
+
+  ngOnInit(): void {
+    this.usersvc.list()
+    .subscribe(res => {
+      console.log(res)
+    });
+  }
+
+  constructor(private usersvc: UserService) {}
 }
