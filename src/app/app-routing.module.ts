6c53c50249f42dca6843e4934b8c707c10d39b91
Robert Mahoney <robert.w.mahoney@gmail.com>
2018-10-29 23:24:42 -0400

    added user routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { UserListComponent } from './user/user-list/user-list.component';
+import { UserCreateComponent } from './user/user-create/user-create.component';
+import { UserDetailComponent } from './user/user-detail/user-detail.component';
+import { UserLoginComponent } from './user/user-login/user-login.component';
+import { UserEditComponent } from './user/user-edit/user-edit.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', redirectTo: '/Users/List', pathMatch: 'full'},
+  {path:'Users/List', component: UserListComponent},
+  {path:'Users/Create', component: UserCreateComponent},
+  {path:'Users/Detail', component: UserDetailComponent},
+  {path:'Users/Login', component: UserLoginComponent},
+  {path:'Users/Edit', component: UserEditComponent},
+  {path:'**', component: UserListComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
