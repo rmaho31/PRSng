7b1b03654af0daad10fae29b61dc59018f9ecc2d
Robert Mahoney <robert.w.mahoney@gmail.com>
2018-10-30 23:58:46 -0400

    setup html and component

--- src/app/user/user-create/user-create.component.ts
+++ src/app/user/user-create/user-create.component.ts
@@ -1,15 +1,34 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router'
+import { UserService } from '../../services/user.service'
+import { User } from '../../classes/user'
 
 @Component({
   selector: 'app-user-create',
   templateUrl: './user-create.component.html',
   styleUrls: ['./user-create.component.css']
 })
+
 export class UserCreateComponent implements OnInit {
 
-  constructor() { }
+  user: User = new User();
+  password2: String = "";
+
+  constructor(private usersvc: UserService,
+              private router: Router) { }
 
   ngOnInit() {
   }
+
+  create() {
+    this.usersvc.add(this.user)
+      .subscribe(res => {
+        if(res.code != 0){
+          alert('Save Failed');
+        }
+        this.router.navigateByUrl('/users/list');
+      })
+  }
+  
 
 }
