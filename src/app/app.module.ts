efec79cfe76b5e91d3aca42b2e4aa3621abe33ee
Robert Mahoney <robert.w.mahoney@gmail.com>
2018-10-30 23:57:14 -0400

    updated imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -9,6 +10,10 @@ import { UserDetailComponent } from './user/user-detail/user-detail.component';
 import { UserEditComponent } from './user/user-edit/user-edit.component';
 import { UserLoginComponent } from './user/user-login/user-login.component';
 import { UserCreateComponent } from './user/user-create/user-create.component';
+import { MenuComponent } from './menu/menu/menu.component';
+import { HomeComponent } from './home/home/home.component';
+import { AboutComponent } from './about/about/about.component';
+import { MenuItemComponent } from './menu/menu/menu-item/menu-item.component';
 
 @NgModule({
   declarations: [
@@ -17,15 +22,20 @@ import { UserCreateComponent } from './user/user-create/user-create.component';
     UserDetailComponent,
     UserEditComponent,
     UserLoginComponent,
-    UserCreateComponent
+    UserCreateComponent,
+    MenuComponent,
+    HomeComponent,
+    AboutComponent,
+    MenuItemComponent
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     AppRoutingModule,
     HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent],
-  exports: [UserListComponent, UserDetailComponent, UserEditComponent, UserLoginComponent, UserCreateComponent]
+  exports: [UserListComponent, UserDetailComponent, UserEditComponent, UserLoginComponent, UserCreateComponent, MenuComponent, HomeComponent, AboutComponent, MenuItemComponent]
 })
 export class AppModule { }
