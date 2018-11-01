b7365532d960bf963188fa41425286df8fa6b9e5
Robert Mahoney <robert.w.mahoney@gmail.com>
2018-10-31 23:36:53 -0400

    added cookie user persistence

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { UserService } from './services/user.service'
-
+import { CookieService } from 'ngx-cookie-service';
+import { SystemService } from './services/system.service';
+import { Router } from '@angular/router';
+import { UserService } from './services/user.service';
+import { User } from './classes/user';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -8,13 +11,27 @@ import { UserService } from './services/user.service'
 })
 export class AppComponent {
   title = 'PRSng';
+  user: User = new User()
 
   ngOnInit(): void {
-    this.usersvc.list()
-    .subscribe(res => {
-      console.log(res)
-    });
+    if (this.syssvc.data.user.loggedIn === false) {
+      this.user.userName = this.cookiesvc.get('userName');
+      this.user.password = this.cookiesvc.get('password');
+      this.usersvc.login(this.user)
+        .subscribe(jsr => {
+          if (jsr.code == 0) {
+            this.syssvc.data.user.loggedIn = true;
+            this.syssvc.data.user.instance = jsr.data[0];
+            this.router.navigateByUrl('/home')
+          } else {
+            this.router.navigateByUrl('/users/login');
+          }
+        });
+      }
   }
 
-  constructor(private usersvc: UserService) {}
+  constructor(private cookiesvc: CookieService,
+    private syssvc: SystemService,
+    private router: Router,
+    private usersvc: UserService) { }
 }
