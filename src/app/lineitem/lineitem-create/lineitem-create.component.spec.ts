597e7341c3e499712eebc1e9eb5c530353cb41e8
Robert Mahoney <robert.w.mahoney@gmail.com>
2018-11-03 07:50:48 -0400

    combined into one render with pr

--- src/app/lineitem/lineitem-create/lineitem-create.component.spec.ts
+++ src/app/lineitem/lineitem-create/lineitem-create.component.spec.ts
@@ -1,20 +1,20 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { LineitemCreateComponent } from './lineitem-create.component';
+import { LineItemCreateComponent } from './lineitem-create.component';
 
 describe('LineitemCreateComponent', () => {
-  let component: LineitemCreateComponent;
-  let fixture: ComponentFixture<LineitemCreateComponent>;
+  let component: LineItemCreateComponent;
+  let fixture: ComponentFixture<LineItemCreateComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ LineitemCreateComponent ]
+      declarations: [ LineItemCreateComponent ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(LineitemCreateComponent);
+    fixture = TestBed.createComponent(LineItemCreateComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
--- src/app/lineitem/lineitem-create/lineitem-create.component.ts
+++ src/app/lineitem/lineitem-create/lineitem-create.component.ts
@@ -1,7 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router'
+import { Router, ActivatedRoute } from '@angular/router'
 import { LineItemService } from '../../services/lineitem.service'
 import { LineItem } from '../../classes/lineitem'
+import { PurchaseRequestService } from 'src/app/services/purchaserequest.service';
+import { ProductService } from 'src/app/services/product.service';
+import { Product } from 'src/app/classes/product';
 
 @Component({
   selector: 'app-lineitem-create',
@@ -12,12 +15,20 @@ import { LineItem } from '../../classes/lineitem'
 export class LineItemCreateComponent implements OnInit {
 
   lineitem: LineItem = new LineItem();
-  password2: String = "";
+  products: Product[]
 
   constructor(private lineitemsvc: LineItemService,
-              private router: Router) { }
+              private router: Router,
+              private purchaserequestsvc: PurchaseRequestService,
+              private productsvc: ProductService,
+              private route: ActivatedRoute) { }
 
   ngOnInit() {
+    console.log(this.route.snapshot.params.id)
+    this.purchaserequestsvc.get(this.route.snapshot.params.id)
+    .subscribe(res => this.lineitem.purchaseRequest = res.data);
+    this.productsvc.list()
+    .subscribe(res => this.products = res.data);
   }
 
   create() {
@@ -26,7 +37,7 @@ export class LineItemCreateComponent implements OnInit {
         if(res.code != 0){
           alert('Save Failed');
         }
-        this.router.navigateByUrl('/lineitems/list');
+        this.router.navigateByUrl('/purchaserequests/list');
       })
   }
   
