2e03939547fb52e351ee29c4e6ae13528925e4c3
Robert Mahoney <robert.w.mahoney@gmail.com>
2018-11-05 22:20:56 -0500

    added direction

--- src/app/pipe/sort.pipe.ts
+++ src/app/pipe/sort.pipe.ts
@@ -1,4 +1,5 @@
 import { Pipe, PipeTransform } from '@angular/core';
+import { isString } from 'util';
 
 @Pipe({
   name: 'sort'
@@ -7,15 +8,24 @@ export class SortPipe implements PipeTransform {
 
   transform(value: any, args?: any): any {
     let arrObjects = value;
-    let column = args;
+    let column = args.sortBy;
+    let direction = args.direction;
 
     return arrObjects.sort((a, b) => {
       let result = 0;
-      if(a[column] > b[column]) 
-        result = 1;
-      else if(a[column] < b[column]) 
-        result = -1;
+      if(isNaN(parseFloat(a[column])) || isNaN(parseFloat(b[column]))) {
+        if(a[column].toLocaleLowerCase() > b[column].toLocaleLowerCase()) 
+        result = 1 * direction;
+      else if(a[column].toLocaleLowerCase() < b[column].toLocaleLowerCase()) 
+        result = -1 * direction;
       return result;
+      } else {
+        if(a[column] > b[column]) 
+          result = 1 * direction;
+        else if(a[column] < b[column]) 
+          result = -1 * direction;
+        return result;
+      }
     });
   }
 
