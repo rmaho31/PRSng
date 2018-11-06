475a95dc7c85b9f0c632db782996c7a1ebdc9676
Robert Mahoney <robert.w.mahoney@gmail.com>
2018-11-05 22:17:37 -0500

    changed initial routing functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,21 +14,7 @@ export class AppComponent {
   user: User = new User()
 
   ngOnInit(): void {
-    if (this.syssvc.loggedIn === false) {
-      this.user.userName = this.cookiesvc.get('userName');
-      this.user.password = this.cookiesvc.get('password');
-      this.usersvc.login(this.user)
-        .subscribe(jsr => {
-          if (jsr.code === 0) {
-            console.log(jsr.data)
-            this.syssvc.loggedIn = true;
-            this.syssvc.user = jsr.data;
-            this.router.navigateByUrl('/home')
-          } else {
-            this.router.navigateByUrl('/users/login');
-          }
-        });
-      }
+    this.router.navigateByUrl('users/login');
   }
 
   constructor(private cookiesvc: CookieService,
